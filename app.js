7029e64a68d5a85ca600ef64f2fc226902faa8d0
Appirio 7895 <Appirio 7895>
2017-07-07 17:31:49 +0530

    second commit

---
+++ app.js
@@ -0,0 +1,112 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const fs = require("fs");
+const multer  = require('multer');
+const pg = require('pg');
+const cors = require('cors')
+
+
+
+
+// const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.use(express.static('public'));
+
+// parse application/json 
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+app.get('/', function (req, res) {
+   res.send('Hello World');
+})
+
+
+
+// This responds with "Hello World" on the homepage
+app.get('/index.htm', (req, res) => {
+   console.log("Got a GET request for the homepage");
+    res.sendFile( __dirname + "/" + "index.htm" );
+})
+
+app.post('/uploadData',(req, res) => {
+    const conString = "postgres://postgres:.@localhost:5432/postgres";
+    const client = new pg.Client(conString);
+    client.connect();
+    console.log("uploadData Box is here");
+    console.log(req.body);
+    client.query(`INSERT INTO public.records(name,mobilenumber,state,email,address,pin,"isChecked") VALUES ('${req.body.name}',${req.body.mobilenumber},'${req.body.state}','${req.body.email}','${req.body.address}',${req.body.pin},${req.body.isChecked})`)
+    .then(() =>{
+    const query1 = client.query('SELECT * FROM public.records')
+    .then((row) =>{
+    console.log(row.rows);
+
+    client.end();
+  return res.send(JSON.stringify(row.rows));
+});
+ }).catch(error => {
+  client.end();
+  res.send(JSON.stringify(error));
+  console.log(error);
+ })
+    
+})
+// Accessing Data from Database
+app.get('/getData',(req, res) => {
+    console.log('test');
+    const conString = "postgres://postgres:.@localhost:5432/postgres";
+    const client = new pg.Client(conString);
+    client.connect();
+    console.log("Get DataBox is here : ");
+    client.query('SELECT * FROM public.records').then((row) =>{
+        console.log(row.rows);
+        console.log('####################################################################');
+        console.log(JSON.stringify(row.rows));
+        client.end();
+        res.send(JSON.stringify(row.rows));
+    }).catch(error => {
+            client.end();
+            res.json(JSON.stringify(error));
+            console.log(error);
+ });
+
+});
+//============
+
+
+app.post('/process_post', function (req, res) {
+
+  client.connect();
+	const results = [];
+   // Prepare output in JSON format
+   response = {
+      first_name:req.body.first_name,
+      last_name:req.body.monile_number
+   };
+   client.query(`INSERT INTO public.records(name,mobilenumber) VALUES ('${req.body.first_name}',${req.body.monile_number})`);
+   const query1 = client.query('SELECT name, mobilenumber FROM records');
+
+  query1.then((row) =>{
+	   console.log(row.rows);
+	//res.send(JSON.stringify(row.rows));
+});
+
+   		query1.on('end', function() {
+    		client.end();
+		});
+
+   // console.log(results);
+   // res.end(JSON.stringify(response));
+})
+
+
+
+var server = app.listen(8081, () =>{
+
+   var host = server.address().address
+   var port = server.address().port
+
+   console.log("Example app listening at http://%s:%s", host, port)
+})
+
