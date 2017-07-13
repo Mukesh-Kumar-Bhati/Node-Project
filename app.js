4fb8ceae97fd0c21c297304c1e12ad47e83f56ec
Appirio 7895 <Appirio 7895>
2017-07-13 10:52:40 +0530

    uploadData endpoint is updated

--- app.js
+++ app.js
@@ -33,23 +33,68 @@ app.get('/index.htm', (req, res) => {
 /////////////////////////////////////////////////////////////
 
 
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, (err, client, done) =>{
-    client.query('SELECT * FROM records', (err, result) =>{
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       {   
-       response.json(result.rows);
-        }
-    });
-  });
+app.get('/db',  (request, response) =>{
+
+    const conString = process.env.DATABASE_URL;
+    const client = new pg.Client(conString);
+    client.connect();
+    client.query(`SELECT * FROM records`).then((row) =>{
+    console.log(row.rows);
+
+    client.end();
+  return res.send(JSON.stringify(row.rows));
+   }).catch(error => {
+  client.end();
+  res.send(JSON.stringify(error));
+  console.log(error);
+ })
+
+  // pg.connect(process.env.DATABASE_URL, (err, client, done) =>{
+  //   client.query('SELECT * FROM records', (err, result) =>{
+  //     done();
+  //     if (err)
+  //      { console.error(err); response.send("Error " + err); }
+  //     else
+  //      {   
+  //      response.json(result.rows);
+  //       }
+  //   });
+  // });
 });
 
 ////////////////////////////////////////////////////////////
 
+//Upload Data on server database
 
+///!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+app.post('/uploadData',(req, res) => {
+    const conString = process.env.DATABASE_URL;
+    const client = new pg.Client(conString);
+    client.connect();
+    console.log("uploadData Box is here");
+    console.log(req.body);
+    client.query(`INSERT INTO public.records(name,mobilenumber,state,email,address,pin,"isChecked") VALUES ('${req.body.name}',${req.body.mobilenumber},'${req.body.state}','${req.body.email}','${req.body.address}',${req.body.pin},${req.body.isChecked})`)
+        .then(() =>{
+              const query1 = client.query('SELECT * FROM public.records')
+             .then((row) =>{
+                  console.log(row.rows);
+
+                  client.end();
+                  return res.send(JSON.stringify(row.rows));
+              });
+          }).catch(error => {
+  client.end();
+  res.send(JSON.stringify(error));
+  console.log(error);
+ })
+    
+})
+
+
+///!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+ // Code for Local host 
+/**
 
 app.post('/uploadData',(req, res) => {
     const conString = "postgres://postgres:.@localhost:5432/postgres";
@@ -72,7 +117,12 @@ app.post('/uploadData',(req, res) => {
   console.log(error);
  })
     
-})
+})   
+
+ **/
+
+
+
 // Accessing Data from Database
 app.get('/getData',(req, res) => {
     console.log('test');
