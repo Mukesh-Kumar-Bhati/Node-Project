14bb2a6dabe118cf9c1e9dd811befe6040ab16f7
Appirio 7895 <Appirio 7895>
2017-07-17 16:37:53 +0530

    add cluster in server

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const fs = require("fs");
 const multer  = require('multer');
 const pg = require('pg');
 const cors = require('cors')
+const cluster = require('cluster');
+const http = require('http');
+const numCPUs = require('os').cpus().length;
 
 
 
@@ -248,11 +251,33 @@ app.post('/process_post', function (req, res) {
 
 
 
-var server = app.listen(app.get('port'), () =>{
+// var server = app.listen(app.get('port'), () =>{
 
-   var host = server.address().address
-   var port = server.address().port
+//    var host = server.address().address
+//    var port = server.address().port
 
-   console.log("Example app listening at http://%s:%s", host, port)
-})
+//    console.log("Example app listening at http://%s:%s", host, port)
+// })
+
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  // Workers can share any TCP connection
+  // In this case it is an HTTP server
+  http.createServer((req, res) => {
+    res.writeHead(200);
+    res.end('hello world\n');
+  }).listen(app.get('port'));
+
+  console.log(`Worker ${process.pid} started`);
+}
 
